a30bd16c60a76f297dadf5927a90b42414a817d9
Lúcio <luciomoriyama@gmail.com>
2023-02-16 20:33:32 -0300

    [BACKEND][CONTROLLER] Adicionando tratamento de respostas de sucesso

--- api/src/main/java/itau/canais/api/modules/produto/controller/ContasController.java
+++ api/src/main/java/itau/canais/api/modules/produto/controller/ContasController.java
@@ -3,10 +3,13 @@ package itau.canais.api.modules.produto.controller;
 import itau.canais.api.modules.produto.dto.DadosConta;
 import itau.canais.api.modules.produto.dto.DadosDepositar;
 import itau.canais.api.modules.produto.dto.DadosListagemContas;
+import itau.canais.api.modules.produto.entities.Conta;
 import itau.canais.api.modules.produto.services.ContaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -18,8 +21,11 @@ public class ContasController {
     private ContaService contaService;
 
     @PostMapping
-    public void cadastrarConta(@RequestBody @Valid DadosConta dadosConta){
+    public ResponseEntity cadastrarConta(@RequestBody @Valid DadosConta dadosConta, UriComponentsBuilder uriBuilder){
+        var conta = new Conta(dadosConta);
         contaService.cadastrarConta(dadosConta);
+        var uri = uriBuilder.path("/contas/{id}").buildAndExpand(conta.getCpf()).toUri();
+        return ResponseEntity.created(uri).body("Cadastrado com sucesso");
     }
 
     @GetMapping
@@ -28,7 +34,9 @@ public class ContasController {
     }
 
     @PutMapping
-    public void depositar(@RequestBody @Valid DadosDepositar dados){
+    public ResponseEntity depositar(@RequestBody @Valid DadosDepositar dados){
         contaService.depositar(dados);
+        return ResponseEntity.ok("Deposito Gerencial efetuado com sucesso");
+
     }
 }
