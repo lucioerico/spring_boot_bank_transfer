d6ff5ab686d8c4b7892ca89368d1c3947955bcdd
Lúcio <luciomoriyama@gmail.com>
2023-02-16 19:50:44 -0300

    [BACKEND][DADOS DE TESTE] Configuração dos banco de dados de testes

---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (nome, cpf, senha) VALUES
+('Cliente 1', '46354480028', 'Senh@023');
+
+INSERT INTO contas (agencia, nconta, saldo, cpf) VALUES
+('001', '00001-1', 1000.00, '46354480028');
