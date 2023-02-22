6949550d59e247c656b45d4e025abef02fadb761
Lúcio <lucioerico@gmail.com>
2023-02-22 12:51:32 -0300

    [BACKEND][OPERACAO] ATUALIZAÇÃO DA CLASSE DADOSTRANSFERENCIA PARA RECEBER A OPERAÇÃO PIX, TED E DOC

--- api/src/main/java/itau/canais/api/modules/produto/dto/DadosTransferir.java
+++ api/src/main/java/itau/canais/api/modules/produto/dto/DadosTransferir.java
@@ -9,10 +9,11 @@ public record DadosTransferir(
         String nconta,
         String cpf,
         BigDecimal saldo,
-        BigDecimal valorTransferencia){
+        BigDecimal valorTransferencia,
+        Operacao operacao){
 
-    public DadosTransferir(Transferencia transferencia){
-        this(transferencia.getAgencia(), transferencia.getNconta(), transferencia.getCpf(), transferencia.getValorTransferencia(), transferencia.getSaldo());
+    public DadosTransferir(Transferencia transferencia, Operacao operacao){
+        this(transferencia.getAgencia(), transferencia.getNconta(), transferencia.getCpf(), transferencia.getValorTransferencia(), transferencia.getSaldo(), operacao);
     }
 }
 
