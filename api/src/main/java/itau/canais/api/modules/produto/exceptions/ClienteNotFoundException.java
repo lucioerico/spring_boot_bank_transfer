2226488848a617db4460688840d5567e77c09577
Lucio <lucioerico@gmail.com>
2023-02-28 19:00:29 -0300

    [PROJETO][REVISÃO]REVISÃO ANTES DA ENTREGA

--- api/src/main/java/itau/canais/api/modules/produto/exceptions/ClienteNotFoundException.java
+++ api/src/main/java/itau/canais/api/modules/produto/exceptions/ClienteNotFoundException.java
@@ -1,4 +1,11 @@
 package itau.canais.api.modules.produto.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class ClienteNotFoundException extends Throwable {
+    public ClienteNotFoundException(String message) {
+        super(message);
+    }
 }
--- api/src/main/java/itau/canais/api/modules/produto/exceptions/SaldoInsuficienteException.java
+++ api/src/main/java/itau/canais/api/modules/produto/exceptions/SaldoInsuficienteException.java
@@ -1,6 +1,11 @@
 package itau.canais.api.modules.produto.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class SaldoInsuficienteException extends Throwable {
-    public SaldoInsuficienteException(String s) {
+    public SaldoInsuficienteException(String message) {
+        super(message);
     }
 }
